ea1302f5b5c0c2e587db03f8f4285473670fba22
taylormichels <taylormichels@gmail.com>
2017-08-09 18:01:21 -0400

    completed exercises

--- SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
+++ SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace SwEngHomework.Commissions
 {
@@ -7,8 +9,55 @@ namespace SwEngHomework.Commissions
     {
         public IDictionary<string, double> CalculateCommissionsByAdvisor(string jsonInput)
         {
-            // TODO: your implementation
-            throw new NotImplementedException();
+            var result = new Dictionary<string, double>();          
+            var input = JsonConvert.DeserializeObject<CommissionData>(jsonInput);
+            foreach (var advisor in input.Advisors)
+            {
+                var allCommmissionByAdvisor = input.Accounts.Where(a => a.Advisor == advisor.Name);
+                decimal total = allCommmissionByAdvisor.Sum(a => a.PresentValue);
+                decimal accountFee = 0;
+                decimal commission = 0;
+
+                if (total < 50000)
+                    accountFee = total * .0005m;
+                else if (total >= 50000 && total < 100000)
+                    accountFee = total * .0006m;
+                else
+                    accountFee = total * .0007m;
+
+                if (advisor.Level == "Junior")
+                    commission = accountFee * .25m;
+                else if (advisor.Level == "Experienced")
+                    commission = accountFee * .5m;
+                else
+                    commission = accountFee;
+
+                result.Add(advisor.Name, Math.Round((double)commission, 2));
+
+            }
+
+            return result;
+        }
+
+        [Serializable]
+        public class Advisors
+        {
+            public string Name { get; set; }
+            public string Level { get; set; }
+        }
+
+        [Serializable]
+        public class Accounts
+        {
+            public string Advisor { get; set; }
+            public decimal PresentValue { get; set; }
+        }
+        
+        [Serializable]
+        public class CommissionData
+        {
+            public List<Advisors> Advisors { get; set; }
+            public List<Accounts> Accounts { get; set; }
         }
     }
 }
--- SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SwEngHomework.DescriptiveStatistics
 {
@@ -6,8 +7,36 @@ namespace SwEngHomework.DescriptiveStatistics
     {
         public Stats Calculate(string semicolonDelimitedContributions)
         {
-            // TODO: your implementation
-            throw new NotImplementedException();
+            string[] contributions = semicolonDelimitedContributions.Split(';');
+            if (contributions.Length == 0)
+                return new Stats { Average = 0, Median = 0, Range = 0 };
+
+            decimal[] values = new decimal[contributions.Length];
+            Stats result = new Stats();
+
+            for (int i = 0; i < contributions.Length; i++)
+            {
+                decimal value = 0m;
+                if (Decimal.TryParse(contributions[i].Replace("$", "").Trim(), out value))
+                    values[i] = value;
+            }
+
+            values = values.Where(val => val != 0).ToArray();
+            Array.Sort(values);
+
+            result.Average = (double) Math.Round(values.Sum() / values.Length, 2);
+            result.Range = (double) Math.Round(values.Max() - values.Min(), 2);
+
+            if (values.Length % 2 == 0)
+            {
+                var middleHigh = (int) values.Length / 2;
+                var middleLow = middleHigh - 1;
+                result.Median = (double) Math.Round((values[middleLow] + values[middleHigh]) / 2, 2);
+            }
+            else
+                result.Median = (double) Math.Round(values[values.Length / 2], 2);
+
+            return result;
         }
     }
 }
